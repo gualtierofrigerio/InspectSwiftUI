0bb20222edde70c7d0cb49cced72a3bbbdf3dd4a
Gualtiero <gualtiero.frigerio@gmail.com>
2022-01-25 16:12:38 +0100

    fix for screenshot

--- InspectSwiftUI/Views/ScreenshotMaker.swift
+++ InspectSwiftUI/Views/ScreenshotMaker.swift
@@ -11,10 +11,11 @@ class ScreenshotMaker: UIView {
     /// Takes the screenshot of the superview of this superview
     /// - Returns: The UIImage with the screenshot of the view
     func screenshot() -> UIImage? {
-        guard let containerView = self.superview?.superview else { return nil }
-        let renderer = UIGraphicsImageRenderer(bounds: containerView.bounds)
+        guard let containerView = self.superview?.superview,
+              let containerSuperview = containerView.superview else { return nil }
+        let renderer = UIGraphicsImageRenderer(bounds: containerView.frame)
         return renderer.image { (context) in
-            containerView.layer.render(in: context.cgContext)
+            containerSuperview.layer.render(in: context.cgContext)
         }
     }
 }
