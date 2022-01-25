7cf80ca47c15a98c0a709e3e197e5e641685fc2a
Gualtiero <gualtiero.frigerio@gmail.com>
2022-01-25 11:55:46 +0100

    renamed ScreenshotMaker

---
+++ InspectSwiftUI/Views/ScreenshotMaker.swift
@@ -0,0 +1,20 @@
+//
+//  ScreenshotMaker.swift
+//  InspectSwiftUI
+//
+//  Created by Gualtiero Frigerio on 21/01/22.
+//
+
+import UIKit
+
+class ScreenshotMaker: UIView {
+    /// Takes the screenshot of the superview of this superview
+    /// - Returns: The UIImage with the screenshot of the view
+    func screenshot() -> UIImage? {
+        guard let containerView = self.superview?.superview else { return nil }
+        let renderer = UIGraphicsImageRenderer(bounds: containerView.bounds)
+        return renderer.image { (context) in
+            containerView.layer.render(in: context.cgContext)
+        }
+    }
+}
