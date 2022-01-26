ec621205df509df25733c5fb522beb2a8d488eef
Gualtiero <gualtiero.frigerio@gmail.com>
2022-01-26 17:40:21 +0100

    HostView now accepts a view builder

--- InspectSwiftUI/Views/TestUIHostingController.swift
+++ InspectSwiftUI/Views/TestUIHostingController.swift
@@ -15,6 +15,15 @@ struct TestView: View {
     }
 }
 
+struct ViewText: View {
+    var body: some View {
+        VStack {
+            Text("Text...")
+                .background(.blue)
+        }
+    }
+}
+
 class HostViewContainerModel: ObservableObject {
     @Published var size: CGSize = .zero
 }
@@ -23,21 +32,28 @@ struct HostViewContainer: View {
     @ObservedObject var model = HostViewContainerModel()
     
     var body: some View {
-        HostView(model: model)
+        HostView(model: model) {
+            VStack {
+                Text("Write whathever you want")
+                Text("It still works")
+            }
+        }
             .frame(width: model.size.width, height: model.size.height)
     }
 }
 
-struct HostView: UIViewControllerRepresentable {
+struct HostView<Content: View>: UIViewControllerRepresentable {
     let model: HostViewContainerModel
+    let content: Content
     
-    init(model: HostViewContainerModel) {
+    init(model: HostViewContainerModel, _ content: @escaping () -> Content) {
         self.model = model
+        self.content = content()
     }
     
     func makeUIViewController(context: Context) -> some UIViewController {
         let text = Text("Text in UIViewControllerRepresentable")
-        let controller = UIHostingController(rootView: text)
+        let controller = UIHostingController(rootView: self.content)
         controller.view.backgroundColor = .orange
         let size = controller.view.intrinsicContentSize
         controller.view.frame = CGRect(origin: .zero, size: size)
@@ -47,7 +63,7 @@ struct HostView: UIViewControllerRepresentable {
     
     func updateUIViewController(_ uiViewController: UIViewControllerType,
                                 context: Context) {
-        
+        print(uiViewController.view)
     }
 }
 
